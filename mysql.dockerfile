d5fd589fb8d0261fc7f499d5751e231ed3b21dbb
Rodrigo Tenorio de Boa Ventura <boaventura19@yahoo.com.br>
2020-01-08 14:30:49 -0300

    ingestao de dados no data.sql e config. application.yml.

--- mysql.dockerfile
+++ mysql.dockerfile
@@ -1,6 +1,11 @@
 FROM mysql:8.0.18
 MAINTAINER RODRIGO TENORIO DE BOA VENTURA<boaventura19@yahoo.com.br>
 ENV MYSQL_ROOT_PASSWORD=Rr123
+ENV MYSQL_USER=root
+ENV MYSQL_PASSWORD=Rr123
 ADD ./banco.sql /docker-entrypoint-initdb.d
-COPY banco.sql /docker-entrypoint-initdb.d/
+#COPY ./banco.sql /docker-entrypoint-initdb.d/
+#RUN mysql -uroot -p${MYSQL_ROOT_PASSWORD} </docker-entrypoint-initdb.d/banco.sql
+ADD schema.sql /docker-entrypoint-initdb.d/schema.sql
 EXPOSE 3306
+CMD ["mysqld"]
---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+
+
+insert into cliente(nome,cpf,data_nascimento,idade) values('Jose maria', '12245608900','2019-11-05',0);
+insert into cliente(nome,cpf,data_nascimento,idade) values('Jose', '11345678900','2019-11-05',0);
+insert into cliente(nome,cpf,data_nascimento,idade) values('Jose', '00000000000','2019-11-05',0);
+insert into cliente(nome,cpf,data_nascimento,idade) values('Rodrigo Tenorio', '12345678900','2019-11-05',0);
+insert into usuario(nome,email,senha) values('Rodrigo Tenorio','rodrigo@com','$2a$10$OVAMbJHd0FpycZ2hPKVQbuPXoAITb4jBM46br8FURDYfgRUJYnMSa');
